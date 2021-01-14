eeb8dd2c89fbd7a3f4380d5a747c9f695252dd7e
Leela Narasimha <leela.narsimha@gmail.com>
2021-01-14 23:48:23 +0530

    Feat: Add new function and class components

---
+++ my-app/src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <div>
+                <div>Hello World By Leela Web Dev</div>
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
