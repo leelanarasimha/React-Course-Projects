ac1c261d5f35a7fac819a5ea69114751dbd9f177
Leela Narasimha <leela.narsimha@gmail.com>
2021-01-15 22:37:23 +0530

    Feat: Add event click handler

--- my-app/src/components/Posts/Posts.js
+++ my-app/src/components/Posts/Posts.js
@@ -10,10 +10,51 @@ class Posts extends Component {
 
         postTitle: 'Posts List',
     };
+
+    updateTitleHandler(title, e) {
+        e.preventDefault();
+        console.log('updatin title');
+        console.log(this);
+        this.setState({
+            postTitle: title,
+        });
+    }
+
+    titleHandler = (title, e) => {
+        e.preventDefault();
+        console.log('updatin title via pro[erty function');
+        console.log(this);
+        this.setState({
+            postTitle: title,
+        });
+    };
+
     render() {
         return (
             <div>
                 <h2 className='text-2xl my-3'>{this.state.postTitle}</h2>
+                <div>
+                    <a
+                        href='http://www.google.com'
+                        onClick={this.titleHandler.bind(
+                            this,
+                            'Leela Web Dev',
+                        )}
+                        className='px-5 py-2 bg-red-500 rounded-3xl text-white'
+                    >
+                        Update with property
+                    </a>
+                    <a
+                        href='http://www.google.com'
+                        onClick={this.updateTitleHandler.bind(
+                            this,
+                            'Modfied',
+                        )}
+                        className='px-5 py-2 bg-red-500 rounded-3xl text-white'
+                    >
+                        Update via method
+                    </a>
+                </div>
                 <hr />
                 <div className='flex my-3'>
                     <SinglePost
