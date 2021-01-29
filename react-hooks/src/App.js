249f73e4f91930797c5b08194abc311bd8840269
Leela Narasimha <leela.narsimha@gmail.com>
2021-01-29 23:09:11 +0530

    IMplement React Hooks

---
+++ react-hooks/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className='container mx-auto'>
+            <div className='border border-gray-400'>Hai Leela</div>
+        </div>
+    );
+}
+
+export default App;
