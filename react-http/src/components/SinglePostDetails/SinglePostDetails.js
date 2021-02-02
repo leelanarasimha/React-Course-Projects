83e9ec3959c938ec7c54986114ac23c68efe899a
Leela Narasimha <leela.narsimha@gmail.com>
2021-02-02 23:35:15 +0530

    Feat: Create Post Data

--- react-http/src/components/SinglePostDetails/SinglePostDetails.js
+++ react-http/src/components/SinglePostDetails/SinglePostDetails.js
@@ -1,16 +1,49 @@
+import axios from 'axios';
 import { Component } from 'react';
 
 export default class SinglePostDetails extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            post: null,
+        };
     }
+
+    componentDidMount() {
+        this.getPostDetails();
+    }
+
+    componentDidUpdate() {
+        if (this.state.post && this.state.post.id === this.props.id) {
+            return;
+        }
+        this.getPostDetails();
+    }
+
+    getPostDetails = () => {
+        axios
+            .get(
+                `https://react-course-b798e-default-rtdb.firebaseio.com/posts/${this.props.id}.json`,
+            )
+            .then((response) => {
+                this.setState({
+                    post: { ...response.data, id: this.props.id },
+                });
+            });
+    };
 
     render() {
         return (
-            <div class='my-2 border border-gray-300 shadow p-4'>
-                <div>Id: </div>
-                <div>Title: </div>
-                <div>Description: </div>
+            <div className='my-2 border border-gray-300 shadow p-4'>
+                {this.state.post && (
+                    <div>
+                        <div>Id: {this.state.post.id}</div>
+                        <div>Title: {this.state.post.title}</div>
+                        <div>
+                            Description: {this.state.post.description}
+                        </div>
+                    </div>
+                )}
             </div>
         );
     }
