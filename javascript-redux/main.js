6938058b5e480a7b22957b8bd1e913db48193aec
Leela Narasimha <leela.narsimha@gmail.com>
2021-02-21 23:23:30 +0530

    Implement redux in vanilla javascript

---
+++ javascript-redux/main.js
@@ -0,0 +1,59 @@
+//initialize state
+const initialState = { counter: 4 };
+
+//create action creators
+const increment = () => {
+    return {
+        type: 'increment',
+    };
+};
+
+const decrement = () => {
+    return {
+        type: 'decrement',
+    };
+};
+
+//reducer
+function counterReducer(state = initialState, action) {
+    if (action.type === 'increment') {
+        return {
+            ...state,
+            counter: state.counter + 1,
+        };
+    }
+
+    if (action.type === 'decrement') {
+        return {
+            ...state,
+            counter: state.counter - 1,
+        };
+    }
+
+    return state;
+}
+
+//create store
+const store = Redux.createStore(counterReducer);
+
+function render() {
+    const state = store.getState();
+
+    console.log(state);
+    document.getElementById('counter').innerHTML = state.counter;
+}
+render();
+
+store.subscribe(render);
+
+document
+    .getElementById('increment')
+    .addEventListener('click', function () {
+        store.dispatch(increment());
+    });
+
+document
+    .getElementById('decrement')
+    .addEventListener('click', function () {
+        store.dispatch(decrement());
+    });
