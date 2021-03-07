e14ccc1d4e57829c247afcf0f09a37457f21b999
Leela Narasimha <leela.narsimha@gmail.com>
2021-03-07 21:59:30 +0530

    Added middleware

--- redux-posts-app/src/store/store.js
+++ redux-posts-app/src/store/store.js
@@ -1,4 +1,17 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import PostsReducer from './reducers/PostsReducer';
 
-export const store = createStore(PostsReducer);
+const loggerMiddleware = (store) => (next) => (action) => {
+    console.log('dispatching action', action);
+    console.log('before dispatching state', store.getState());
+    let result = next(action);
+    setTimeout(() => {
+        console.log('dispatch time out');
+    }, 5000);
+    console.log('next state', store.getState());
+    return result;
+};
+
+const middleware = applyMiddleware(loggerMiddleware);
+
+export const store = createStore(PostsReducer, middleware);
